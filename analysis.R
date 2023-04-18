4c8c353301bcb995331ae69f6f9e31f9cc46e48b
boa50 <bruno.albuquerque50@gmail.com>
2023-04-19 04:34:45 +0930

    Created some checks that need to be made during the exploration

--- analysis.R
+++ analysis.R
@@ -11,3 +11,8 @@ df_hunger_gdp <- read.csv("data/global-hunger-index-vs-gdp-per-capita.csv",
 
 df_hunger_gdp <- df_hunger_gdp %>% 
   filter(year >= 1900)
+
+# Check why some countries don't have a hungry index
+# Check the minimum value of the index
+# Check if there is some relationship between gdp per capita and hungry index
+# Check where (continent) the hungry is still strong
